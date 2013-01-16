5c7d3ac090f01af70b9b86e1d104acccd7a38ffd
iamhahar <iamhahar@gmail.com>
2013-01-16 19:42:21 +0800

    add thread

---
+++ a.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#read arg1;
+#ls $arg1;
+#./nvflash -r
+echo "start formation"
+#sudo mkfs -t ext4 /dev/sdd1
+#sudo mkfs.ntfs /dev/sdd1
+sudo -s
--- src/neo/dut/thread/dutTest.java
+++ src/neo/dut/thread/dutTest.java
@@ -54,28 +54,78 @@ public class dutTest  extends Thread {
 		 });	
 	}
 	private void runScriptFile(){
-		try {
-			Process p = Runtime.getRuntime().exec(this.scriptName);
+		//try {
+		 	ProcessBuilder builder = new ProcessBuilder();  
+	        builder.command(this.scriptName);  
+	        final Process p;
+			try {
+				p = builder.start();
+				new Thread() {  
+	                public void run() {  
+	                    BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));  
+	                    try {  
+	                        String lineB = null;  
+	  
+	                        while ((lineB = br.readLine()) != null) {  
+	                            if (lineB != null) System.out.println(lineB);  
+	  
+	                        }  
+	                    } catch (IOException e) {  
+	                        e.printStackTrace();  
+	                    }  
+	                }  
+	            }.start();  
+	            
+	            new Thread() {  
+	                public void run() {  
+	                    BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream()));  
+	                    try {  
+	                        String lineB = null;  
+	  
+	                        while ((lineB = br.readLine()) != null) {  
+	                            if (lineB != null) System.out.println(lineB);  
+	  
+	                        }  
+	                    } catch (IOException e) {  
+	                        e.printStackTrace();  
+	                    }  
+	                }  
+	            }.start();
+	              
+	            p.waitFor();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			//Process p = Runtime.getRuntime().exec(this.scriptName);
 			//Process p = Runtime.getRuntime().exec();
-			BufferedWriter buffOut = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
-            BufferedReader buffIn = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            BufferedReader buffErrIn = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			//BufferedWriter buffOut = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+            //BufferedReader buffIn = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            //BufferedReader buffErrIn = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             /*
             //Pipe args
             buffOut.write("/ \n");
 			buffOut.flush();
 			*/
 			//Grep message
+            //p.waitFor();
+ catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            
+                    
+          /*  
 			String line = "";
             while((line = buffIn.readLine()) != null){
             	String err = "";
             	if((err = buffErrIn.readLine()) != null) {
             		if (err.contains("sudo:"))  {
-            			buffOut.write("000000");
-            			buffOut.newLine();
-            			buffOut.flush();
+            		//	buffOut.write("000000");
+            		//	buffOut.newLine();
+            		//	buffOut.flush();
             		}
-            		System.out.println(err);
+            	//	System.out.println(err);
             	}
                     System.out.println(line);
             }
@@ -85,6 +135,10 @@ public class dutTest  extends Thread {
            	} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
+           	} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			*/	
 	}
 }
